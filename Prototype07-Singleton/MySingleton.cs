6d939fdaeb4f090e32d599f7527498b6917d9053
SomilaY <ST10091012@vcconnect.edu.za>
2023-10-01 12:15:08 +0000

    Add project files.

---
+++ Prototype07-Singleton/MySingleton.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype07_Singleton
+{
+    public sealed class MySingleton
+    {
+        private static MySingleton instance = null;
+        private static readonly object padlock = new object();
+
+        private MySingleton()
+        {
+            // Private constructor to prevent external instantiation
+        }
+
+        public static MySingleton Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new MySingleton();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        public void DoSomething()
+        {
+            // Method of the Singleton class
+            Console.WriteLine("Doing something...");
+            Console.ReadLine();
+        }
+    }
+
+}
---
+++ Prototype07-Singleton/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype07_Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MySingleton instance = MySingleton.Instance;
+
+            // Call a method on the Singleton instance
+            instance.DoSomething();
+
+        }
+    }
+}
